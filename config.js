48418b86c3bdff34233d2cf5a48e747d7b2747de
dhyey patel <145635084+Dhyey7773@users.noreply.github.com>
2026-06-01 19:05:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://pyqnhbuzpagosqxsjvwg.supabase.co",
-  SUPABASE_ANON_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5cW5oYnV6cGFnb3NxeHNqdndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3OTc1NzksImV4cCI6MjA5NTM3MzU3OX0.XorO-8qaHlgpDsPUqcOv6WDxP0op_hWAQ73rVHf28qo"
+  SUPABASE_ANON_KEY: "sb_publishable_SzfOXVnE2HjjdZ4oNptiLQ_oPtZq..."  // ← new key
 };
